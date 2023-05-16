f3c38a2f59f4ed4bdc8597e4394e2e67d24cb89d
James Bruce <jab3@korazin.net>
2023-05-15 23:01:26 -0500

    Remove a lot of old code used for testing
    I'm not sure I'm happy with how this is written. I may try the Stockfish
    class again, or re-write this from scratch. As mentioned in the previous
    commit, there is some hackery happening - we push the move before we
    actually evaluate the position. Somehow it works as I expect it to. It
    points me to the correct moves, but it really shouldn't. It should be
    looking at the next position. I don't understand and it's annoying.

--- run_analysis.py
+++ run_analysis.py
@@ -41,6 +41,7 @@ def parse_arguments():
     parser.add_argument("-f", "--file", help="PGN file to parse")
     parser.add_argument("-e", "--eval", action="store_true", help="Find evaluation swings")
     parser.add_argument("-l", "--list", action="store_true", help="Live moves")
+    parser.add_argument("-n", "--print-fen", action="store_true", help="Print FEN")
     parser.add_argument("-d", "--depth", default=20, help="Depth from which to do analysis")
     parser.add_argument("-t", "--time", default=7.0, help="Set minimum move time for evaluation")
     parser.add_argument("-s", "--show-best", action="store_true", help="Show best move at swing")
@@ -82,26 +83,8 @@ def get_piece_at_square(board, square):
 
 # Most of this probably needs to be moved to another function and have this
 # function simply return the results of get_evaluation()
-#def get_eval(engine, fen):
 def get_eval(engine, board, limits):
-    #e = stockfish.get_evaluation()
     return engine.analyse(board, limits)
-    #v = e['value']
-    #t = e['type']
-    #v = info['score'].relative
-    #print(f"get_eval returned {v} for info['score'].relative")
-
-    #return str(v)
-#    if t == 'cp':
-#        # Leave as centipawns and allow caller to handle any desired
-#        # conversions to pawn units.
-#        # Also - leave it as a string so the return value type remains the same
-#        # regardless of what is returned, so caller has to handle the difference.
-#        return str(v)
-#    elif t == 'mate':
-#        color = "Black" if v < 0 else "White"
-#        av = abs(int(v))
-#        return f"M{av} for {color}"
 
 def print_position_info(s, f, v):
     print(f"Evaluation: {v}")
@@ -117,14 +100,8 @@ else:
     print("Using the test PGN file")
     pgn = open("test_game.pgn")
 
-#if config['depth']:
-#    set_depth(engine, config['depth'])
-#if config['time']:
-#    set_move_time_min(engine, config['time'])
 limits = chess.engine.Limit(time=config['time'], depth=config['depth'])
 
-#print("Options available:")
-#print(engine.options["Hash"])
 print("Config options:")
 print(f"{config}\n")
 
@@ -135,26 +112,21 @@ engine_valuations = {}
 if config['eval']:
     previous_valuation = 0
     for move in schach.mainline_moves():
-        # Make the move on the board and update to next ply
         san = board.san(move)
         turn = board.turn
         move_num = board.fullmove_number # i.e., not the ply
         ply = board.ply()
 
+        # This has to be a hack and wrong. I save the information for the current
+        # move, like SAN and turn and whatnot, but if I don't put this call here
+        # the evaluated position is somehow off by one based on what I see in 
+        # ChessBase.
         board.push(move)
-        if board.outcome():
-            os._exit(1)
 
-        #set_pos(fen)
-        #valuation = get_eval(fen)
-        #eval_info = get_eval(engine, board, float(config['time']), config['depth'])
         eval_info = get_eval(engine, board, limits)
         valuation = str(eval_info['score'].relative)
         engine_valuations[ply] = eval_info
 
-#        print(f"Move = {board.san(move)}")
-#        print(f"pv = {eval_info['pv']}")
-        color = "White" if turn == chess.WHITE else "Black"
         if is_an_int(valuation):
             valuation = int(valuation)
 
@@ -162,71 +134,72 @@ if config['eval']:
             # it currently works, + is good for the current color; - for the other. So a +
             # score for Black means Black is better. A - score for Black means that White
             # is better. But we generally use - for favoring Black and + favoring White.
-            if turn == chess.BLACK:
+            if board.turn == chess.BLACK:
                 valuation = -valuation
 
             eval_diff = abs(valuation - previous_valuation)
 
-#            if board.turn == chess.WHITE: print(f"{move_num}: ", end="")
-#            print(f"{board.san(move)} ", end="")
-#            if board.turn == chess.BLACK: print("")
+            if config['list']:
+                if turn == chess.WHITE: print(f"{move_num}: ", end="")
+                print(f"{san} ", end="")
+                if turn == chess.BLACK: print("")
 
-            if turn == chess.WHITE:
-                print(f"{move_num}:  {san} (fen: {board.fen()})")
-            else:
-                print(f"...: {san} (fen: {board.fen()})")
+            # For printing the FEN, each move on separate line
+            if config['print_fen']:
+                if turn == chess.WHITE:
+                    print(f"{move_num}:  {san} (fen: {board.fen()})")
+                else:
+                    print(f"...: {san} (fen: {board.fen()})")
 
             if eval_diff > VALUATION_THRESHOLD_CP:
-#                print(f"BestMove = {chess.engine.BestMove}")
-                print(f"\nValuation swing at move {move_num}, {san} ({valuation})", end='')
+                print(f"Valuation swing at move {move_num}, {san} ({valuation})", end='')
 
-                print(f"curr ply: {eval_info} --> (move {san})")
-                if board.ply() > 1: print(f"prev ply: {engine_valuations[ply-1]}")
-                print(f"curr valuation = {valuation}")
-                print(f"prev valuation = {previous_valuation}")
-                print("")
+#                print(f"curr ply: {eval_info} --> (move {san})")
+#                if board.ply() > 1: print(f"prev ply: {engine_valuations[ply-1]}")
+#                print(f"curr valuation = {valuation}")
+#                print(f"prev valuation = {previous_valuation}")
+#                print("")
 
                 if config['show_best']:
                     # This isn't right. This shows the next move in this
                     # position, not what should have been played instead of the
                     # move that caused this evaluation swing.
                     #print(f". Engine recommendation: {best_move(fen)}.")
-                    print(f". Engine recommendation: {engine_valuations[ply-1]['pv'][1]}.")
+                    print(f". Engine recommendation: {engine_valuations[ply-2]['pv'][1]}.")
                 else:
                     print('')  # newline
             previous_valuation = valuation
         else:
             print(f"{valuation} at move {move_num}, {san}")
-
-#        if move_num == 5:
-#            print(f"Exiting on move {move_num}")
-#            os._exit(1)
+    engine.close()
+#    exit(1)
 elif config['list']:
     for move in schach.mainline_moves():
         print(f"move = {move}; san = {board.san(move)}")
         board.push(move)
         if board.outcome():
             os._exit(1)
+    engine.close()
 else:
-# FEN for position in move 53:
-# 6k1/R7/5p2/5P1P/3KP1P1/6b1/8/8 b - - 0 53
-# SF in CB evaluates this to 0.00
-#
-# FEN for position in ...52:
-# 6k1/R7/5p2/5P1P/3pP1P1/3K2b1/8/8 w - - 0 53
-# 6k1/R7/5p2/5P1P/3pP1P1/3K2b1/8/8 w - - 0 53
-# SF in CB evaluates this to +18 or so
+    print("Nothing to do. Did you provide an action?")
 
-    print("Evalation info for move 52, Bg3:")
-    board = chess.Board("6k1/R7/5p2/5P1P/3pP1P1/3K2b1/8/8 w - - 0 53")
-    info  = engine.analyse(board, chess.engine.Limit(depth=20))
-    print(info)
-
-    print("Evalation info for move 53, Kxd4:")
-    # The position represented in FEN
-    board = chess.Board("6k1/R7/5p2/5P1P/3KP1P1/6b1/8/8 b - - 0 53")
-    info  = engine.analyse(board, chess.engine.Limit(depth=20))
-    print(info)
-
-
-    #print("Nothing to do. Did you provide an action?")
+# # Some debugging stuff I want to leave
+# # FEN for position in move 53:
+# # 6k1/R7/5p2/5P1P/3KP1P1/6b1/8/8 b - - 0 53
+# # SF in CB evaluates this to 0.00
+# #
+# # FEN for position in ...52:
+# # 6k1/R7/5p2/5P1P/3pP1P1/3K2b1/8/8 w - - 0 53
+# # 6k1/R7/5p2/5P1P/3pP1P1/3K2b1/8/8 w - - 0 53
+# # SF in CB evaluates this to +18 or so
+# 
+# print("Evalation info for move 52, Bg3:")
+# board = chess.Board("6k1/R7/5p2/5P1P/3pP1P1/3K2b1/8/8 w - - 0 53")
+# info  = engine.analyse(board, chess.engine.Limit(depth=20))
+# print(info)
+# 
+# print("Evalation info for move 53, Kxd4:")
+# # The position represented in FEN
+# board = chess.Board("6k1/R7/5p2/5P1P/3KP1P1/6b1/8/8 b - - 0 53")
+# info  = engine.analyse(board, chess.engine.Limit(depth=20))
+# print(info)
