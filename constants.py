232866ecfd37d56b170f8d9a54eec44ccf6607a0
James Bruce <jab3@korazin.net>
2023-05-19 17:11:17 -0500

    Add constants for assigning (arbitrary) values to Mates

--- constants.py
+++ constants.py
@@ -5,6 +5,10 @@ CP_INACCURACY = 40
 CP_MISTAKE    = 90
 CP_BLUNDER    = 200
 
+# Totally arbitrary
+MATE_IN_ONE_CP = 250000
+MATE_CP_SCALE  = 1000
+
 # Centipawn Blunder Categories
 class Category(Enum):
     INVALID    = 0x00
