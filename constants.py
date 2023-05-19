a78c2e549acda2ac72d5949c12b0439deadaa08a
James Bruce <jab3@korazin.net>
2023-05-18 23:18:14 -0500

    Remove Moves enum and make thresholds constants
    Can't compare a number to an enum without using the .value member, and
    that's just weird. So I reverted to using regular constants for the
    centipawn thresholds, but keeping the categories as Enums. There is no
    need for the Moves anymore.

--- constants.py
+++ constants.py
@@ -1,17 +1,14 @@
 from enum import Enum
 
-#VALUATION_THRESHOLD_CP = 1.25*100
+VALUATION_THRESHOLD_CP = 1.25*100
+CP_INACCURACY = 40
+CP_MISTAKE    = 90
+CP_BLUNDER    = 200
 
-class Moves(Enum):
+# Centipawn Blunder Categories
+class Category(Enum):
     INVALID    = 0x00
     OK         = 0x01
     INACCURATE = 0x02
     MISTAKE    = 0x04
     BLUNDER    = 0x08
-
-# Centipawn Blunder Categories
-class Category(Enum):
-    THRESHOLD  = 1.25*100
-    INACCURACY = 40
-    MISTAKE    = 90
-    BLUNDER    = 200
