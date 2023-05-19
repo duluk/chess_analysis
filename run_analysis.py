d76e29da5f3ac6de3d9ee844e0f1a286323d7a2a
James Bruce <jab3@korazin.net>
2023-05-18 22:39:31 -0500

    Refactor into initial class structure
    Trying to make it more general and easier to manage. Maybe. Want to be
    able to use different classes without a hassle. Need to improve the
    configuration, including adding a config file.
    
    This commit should be functionally equivalent to the last commit, though
    there are probably a few bugs with the re-implementation I haven't found.

--- run_analysis.py
+++ run_analysis.py
@@ -6,6 +6,14 @@
 # 2. Perhaps keep up with more than just the recommended move so other
 #    information can be accessed later.
 # 3. Add option for which engine to use, though this will likely require a
+#    config file so engines and paths can be provided on a per-system basis
+# 4. Add another centipawn evaluation/analysis function. What I'm doing now is
+#    comparing CP difference from one move to the next. What if I compared the
+#    CP difference from the player's move to the engine's top chioce? I think
+#    this is what the ChessBase Centipawn Analysis feature does. If not, it's
+#    still interesting. I'm not sure how significant the different methods are,
+#    but something to consider.
+# 5. Fix the configuration abstraction. It's all wonky.
 
 #This could be useful:
 #white_pieces = {'Pawn' : "♙", 'Rook' : "♖", 'Knight' : "♘", 'Bishop' : "♗", 'King' : "♔", 'Queen' : "♕" }
@@ -22,39 +30,197 @@ import chess.engine
 import logging
 logging.basicConfig(level=logging.INFO)
 
-STOCKFISH_BIN='/usr/bin/stockfish'
+import constants as const
+import config    as conf
 
-VALUATION_THRESHOLD_CP = 1.25*100
-INACCURACY_CP = 40
-MISTAKE_CP    = 90
-BLUNDER_CP    = 200
-MOVE_OK    = 0x00
-MOVE_INACCURATE = 0x01
-MOVE_MISTAKE    = 0x02
-MOVE_BLUNDER    = 0x04
+class Arguments:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Arg Parse Stuff")
+        self.arguments = self.parse_arguments()
+        self.args = vars(self.arguments)
 
-# Can set the strength of Stockfish to something more comparable to the ELO of
-# the players in the game so stockfish evaluates based on that ELO. Could be
-# useful. (actually not sure this affects position evaluation)
-#stockfish = Stockfish(path=STOCKFISH_BIN, parameters={"Threads": 6, "UCI_LimitStrength": "true", "UCI_Elo": 1000})
+    def parse_arguments(self):
+        self.parser.add_argument("-f", "--file", help="PGN file to parse")
+        self.parser.add_argument("-e", "--eval", action="store_true", help="Find evaluation swings")
+        self.parser.add_argument("-l", "--list", action="store_true", help="Live moves")
+        self.parser.add_argument("-n", "--print-fen", action="store_true", help="Print FEN")
+        self.parser.add_argument("-d", "--depth", help="Depth from which to do analysis")
+        self.parser.add_argument("-t", "--time", help="Set minimum move time for evaluation")
+        self.parser.add_argument("-s", "--show-best", action="store_true", help="Show best move at swing")
+        # Positional arguments if wanted:
+        # self.parser.add_argument("src", help="source")
+        # self.parser.add_argument("dst", help="dest")
+        return self.parser.parse_args()
 
-# Default to max strength
-#stockfish = Stockfish(path=STOCKFISH_BIN, parameters={"Threads": 6, "UCI_LimitStrength": "false"})
-engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_BIN)
+    @property
+    def args(self):
+        return self._args
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Arg Parse Stuff")
-    parser.add_argument("-f", "--file", help="PGN file to parse")
-    parser.add_argument("-e", "--eval", action="store_true", help="Find evaluation swings")
-    parser.add_argument("-l", "--list", action="store_true", help="Live moves")
-    parser.add_argument("-n", "--print-fen", action="store_true", help="Print FEN")
-    parser.add_argument("-d", "--depth", help="Depth from which to do analysis")
-    parser.add_argument("-t", "--time", help="Set minimum move time for evaluation")
-    parser.add_argument("-s", "--show-best", action="store_true", help="Show best move at swing")
-    # Positional arguments if wanted:
-    # parser.add_argument("src", help="source")
-    # parser.add_argument("dst", help="dest")
-    return vars(parser.parse_args())
+    @args.setter
+    def args(self, value):
+        self._args = value
+
+class Engine_Analysis:
+    def __init__(self, binary = None):
+        self.binary = binary
+
+    def set_depth(self, d):
+        return "Not implemented"
+
+    def set_move_time_min(self, t):
+        return "Not implemented"
+
+    def moves(self):
+        return "Not implemented"
+
+    def get_eval(self, e, b, l):
+        return "Not implemented"
+
+    def set_position(self, fen):
+        return "Not implemented"
+
+    def best_move_fen(self, fen):
+        return "Not implemented"
+
+    def print_position_info(self, s, f, v):
+        return "Not implemented"
+
+class Stockfish_PythonChess(Engine_Analysis):
+    def __init__(self, args, binary=conf.DEFAULT_STOCKFISH_BIN):
+        Engine_Analysis.__init__(self, binary)
+        self.args = args
+        self.engine = chess.engine.SimpleEngine.popen_uci(self.binary)
+
+        # TODO: args.args is ridiculous; fix this
+        self.pgn_file = self.args.args['file']
+        if self.pgn_file:
+            self.pgn = open(self.pgn_file)
+        else:
+            print("Using the test PGN file")
+            self.pgn = open("test_game.pgn")
+
+        if self.args.args['depth']:
+            self.set_depth(self.args.args['depth'])
+        if self.args.args['time']:
+            self.set_move_time_min(self.args.args['time'])
+
+        self.game = chess.pgn.read_game(self.pgn)
+        self.board = self.game.board()
+
+        #print("Config options:")
+        #print(config.args)
+
+    @property
+    def game(self):
+        return self._game
+
+    @game.setter
+    def game(self, value):
+        self._game = value
+
+    @property
+    def board(self):
+        return self._board
+
+    @board.setter
+    def board(self, value):
+        self._board = value
+
+    @property
+    def engine(self):
+        return self._engine
+
+    @engine.setter
+    def engine(self, value):
+        self._engine = value
+
+    def moves(self):
+        return self.game.mainline_moves()
+
+    def run_centipawn(self):
+        #print(f"run_centipawn(self) - {self.args.args['eval']}")
+        return self.args.args['eval'] == True
+
+    def run_list_moves(self):
+        #print(f"run_list_moves(self) - {self.args.args['list']}")
+        return self.args.args['list'] == True
+
+    def set_depth(self, d):
+        # I don't know if this is python-chess or Stockfish
+        #try:
+        #    self.engine.configure({"depth": d})
+        #except:
+        #    print("Invalid option. Available options:")
+        #    print(self.engine.options["Hash"])
+        #    os._exit(1)
+
+        # But this should work with python-chess
+        chess.engine.Limit.depth = int(d)
+
+    def set_move_time_min(self, t):
+        chess.engine.Limit.time = float(config['time'])
+
+    def get_piece_at_square(self, square):
+        return self.board.piece_at(square).symbol()
+
+    # Most of this probably needs to be moved to another function and have this
+    # function simply return the results of get_evaluation()
+    def get_eval(self):
+        # TODO: what is the right/best way to handle the `chess.engine.Limit`
+        # thing? Is there no way to configure this per instance of engine?
+        return self.engine.analyse(self.board, chess.engine.Limit)
+
+    def print_position_info(self, s, f, v):
+        return "Not implemented"
+        #print(f"Evaluation: {v}")
+        #print(f"Move made: {s.move}\n")
+        #print(f"Next Best Move: {best_move(f)}")
+
+class Stockfish_Stockfish(Engine_Analysis):
+    def __init__(self, binary=conf.DEFAULT_STOCKFISH_BIN):
+        Engine_Analysis.__init__(self, binary)
+
+        # Can set the strength of Stockfish to something more comparable to the ELO of
+        # the players in the game so stockfish evaluates based on that ELO. Could be
+        # useful. (actually not sure this affects position evaluation)
+        engine_weak = Stockfish(path=STOCKFISH_BIN, parameters={"Threads": 6, "UCI_LimitStrength": "true", "UCI_Elo": 1000})
+
+        # Default to max strength
+        engine_strong = Stockfish(path=STOCKFISH_BIN, parameters={"Threads": 6, "UCI_LimitStrength": "false"})
+
+        self.set_engine('Strong')
+
+    @property
+    def engine(self):
+        return self._engine
+
+    @engine.setter
+    def engine(self, value):
+        self._engine = value
+
+    # TODO: should this be an enum or constant?
+    def set_engine(self, strong_weak = "Strong"):
+        if strong_weak == 'Strong':
+            self.engine = engine_strong
+        elif strong_weak == 'Weak':
+            self.engine = engine_weak
+        else:
+            raise f"Invalid option given for set_engine: {strong_weak}"
+
+    def set_depth(self, d):
+        engine.set_depth(d)
+
+    def set_move_time_min(self, t):
+        engine.update_engine_parameters({"Minimum Thinking Time": t})
+        # I don't know which class this is supposed to be for:
+        #engine.configure({"Minimum Thinking Time": t})
+
+    def set_position(self, fen):
+        engine.set_fen_position(fen)
+
+    def best_move_fen(fen):
+        if stockfish.is_fen_valid(fen):
+            return stockfish.get_best_move()
 
 def is_an_int(n):
     try:
@@ -63,121 +229,81 @@ def is_an_int(n):
     except ValueError:
         return False
 
-def set_depth(engine, d):
-    #stockfish.set_depth(d)
-    try:
-        engine.configure({"depth": d})
-    except:
-        print("Invalid option. Available options:")
-        print(engine.options["Hash"])
-        os._exit(1)
-    
+def evaluate_centipawns(cp_diff):
+    if cp_diff > const.BLUNDER_CP:
+        return const.MOVE_BLUNDER
+    elif cp_diff > const.MISTAKE_CP:
+        return const.MOVE_MISTAKE
+    elif cp_diff > const.INACCURACY_CP:
+        return const.MOVE_INACCURATE
+    else:
+        return const.MOVE_OK
 
-def set_move_time_min(engine, t):
-    #stockfish.update_engine_parameters({"Minimum Thinking Time": t})
-    engine.configure({"Minimum Thinking Time": t})
+args = Arguments()
 
-#def set_pos(fen):
-#    stockfish.set_fen_position(fen)
-
-#def best_move(fen):
-#    if stockfish.is_fen_valid(fen):
-#        return stockfish.get_best_move()
-
-def get_piece_at_square(board, square):
-    return board.piece_at(square).symbol()
-
-# Most of this probably needs to be moved to another function and have this
-# function simply return the results of get_evaluation()
-def get_eval(engine, board, limits):
-    return engine.analyse(board, limits)
-
-def print_position_info(s, f, v):
-    print(f"Evaluation: {v}")
-    print(f"Move made: {s.move}\n")
-    print(f"Next Best Move: {best_move(f)}")
-
-def evaluate_centipawns(diff):
-    if eval_diff > BLUNDER_CP:
-        return MOVE_BLUNDER
-    elif eval_diff > MISTAKE_CP:
-        return MOVE_MISTAKE
-    elif eval_diff > INACCURACY_CP:
-        return MOVE_INACCURATE
-
-
-config = parse_arguments()
-
-pgn_file = config['file']
-if pgn_file:
-    pgn = open(pgn_file)
-else:
-    print("Using the test PGN file")
-    pgn = open("test_game.pgn")
-
-if config['depth']:
-    chess.engine.Limit.depth = int(config['depth'])
-if config['time']:
-    chess.engine.Limit.time = float(config['time'])
-
-print("Config options:")
-print(config)
-
-schach = chess.pgn.read_game(pgn)
-board  = schach.board()
+schach = Stockfish_PythonChess(args)
 engine_valuations = {}
 
-if config['eval']:
+#print(f"config: {schach.args.args}")
+
+if schach.run_centipawn():
     previous_valuation = 0
-    for move in schach.mainline_moves():
-        san = board.san(move)
-        turn = board.turn
-        move_num = board.fullmove_number # i.e., not the ply
-        ply = board.ply()
+    for move in schach.moves():
+        # Make each of these a getter that uses board; so schach.san(move) would be
+        # implemented as "return self.board.san(move)"
+        san = schach.board.san(move)
+        turn = schach.board.turn
+        move_num = schach.board.fullmove_number # i.e., not the ply
+        ply = schach.board.ply()
 
         # This has to be a hack and wrong. I save the information for the current
         # move, like SAN and turn and whatnot, but if I don't put this call here
         # the evaluated position is somehow off by one based on what I see in 
         # ChessBase.
-        board.push(move)
+        schach.board.push(move)
 
-        eval_info = get_eval(engine, board, chess.engine.Limit)
+        #eval_info = get_eval(engine, board, chess.engine.Limit)
+        eval_info = schach.get_eval()
         valuation = str(eval_info['score'].relative)
         engine_valuations[ply] = eval_info
 
         if is_an_int(valuation):
             valuation = int(valuation)
 
+            # TODO: pretty sure this is wrong. Relative score is relative to each player,
+            # so a positive score is good for that player, and negative is bad. It applies
+            # to both white and black.
             # Keep consistent with White eval: + good for White; - good for Black. The way
             # it currently works, + is good for the current color; - for the other. So a +
             # score for Black means Black is better. A - score for Black means that White
             # is better. But we generally use - for favoring Black and + favoring White.
-            if board.turn == chess.BLACK:
+            if schach.board.turn == chess.BLACK:
                 valuation = -valuation
 
             eval_diff = abs(valuation - previous_valuation)
 
-            if config['list']:
+            if schach.args.args['list']:
                 if turn == chess.WHITE: print(f"{move_num}: ", end="")
                 print(f"{san} ", end="")
                 if turn == chess.BLACK: print("")
 
             # For printing the FEN, each move on separate line
-            if config['print_fen']:
+            if schach.args.args['print_fen']:
                 if turn == chess.WHITE:
                     print(f"{move_num}:  {san} (fen: {board.fen()})")
                 else:
                     print(f"...: {san} (fen: {board.fen()})")
 
             cp_category = evaluate_centipawns(eval_diff)
-            if cp_category == MOVE_INACCURATE:
+            if cp_category == const.MOVE_INACCURATE:
                 print("Inaccuracy ", end='')
-            elif cp_category == MOVE_MISTAKE:
+            elif cp_category == const.MOVE_MISTAKE:
                 print("Mistake ", end='')
-            elif cp_category == MOVE_BLUNDER:
+            elif cp_category == const.MOVE_BLUNDER:
                 print("Blunder ", end='')
 
-            if cp_category != MOVE_OK:
+            if cp_category != const.MOVE_OK:
+                #    print(f"cp_category = {cp_category}")
                 san = f"...{san}" if turn == chess.BLACK else san
                 print(f"at move {move_num}, {san} ({valuation})", end='')
         
@@ -187,7 +313,7 @@ if config['eval']:
                 # print(f"prev valuation = {previous_valuation}")
                 # print("")
         
-                if config['show_best']:
+                if schach.args.args['show_best']:
                     # This isn't right. This shows the next move in this
                     # position, not what should have been played instead of the
                     # move that caused this evaluation swing.
@@ -199,12 +325,12 @@ if config['eval']:
             previous_valuation = valuation
         else:
             print(f"{valuation} at move {move_num}, {san}")
-    engine.close()
-elif config['list']:
-    for move in schach.mainline_moves():
-        print(f"move = {move}; san = {board.san(move)}")
-        board.push(move)
-    engine.close()
+    schach.engine.close()
+elif schach.run_list_moves():
+    for move in schach.moves():
+        print(f"move = {move}; san = {schach.board.san(move)}")
+        schach.board.push(move)
+    schach.engine.close()
 else:
     print("Nothing to do. Did you provide an action?")
 
