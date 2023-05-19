a01c2b8c1ed81e91667d6f8aeda58650146c8a67
James Bruce <jab3@korazin.net>
2023-05-18 22:52:42 -0500

    Use enums instead of constants

--- constants.py
+++ constants.py
@@ -1,11 +1,17 @@
-VALUATION_THRESHOLD_CP = 1.25*100
+from enum import Enum
 
-MOVE_INVALID    = 0x00
-MOVE_OK         = 0x01
-MOVE_INACCURATE = 0x02
-MOVE_MISTAKE    = 0x04
-MOVE_BLUNDER    = 0x08
+#VALUATION_THRESHOLD_CP = 1.25*100
 
-INACCURACY_CP   = 40
-MISTAKE_CP      = 90
-BLUNDER_CP      = 200
+class Moves(Enum):
+    INVALID    = 0x00
+    OK         = 0x01
+    INACCURATE = 0x02
+    MISTAKE    = 0x04
+    BLUNDER    = 0x08
+
+# Centipawn Blunder Categories
+class Category(Enum):
+    THRESHOLD  = 1.25*100
+    INACCURACY = 40
+    MISTAKE    = 90
+    BLUNDER    = 200
